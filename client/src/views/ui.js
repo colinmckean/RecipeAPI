4ae7ad971aaa678dd98a5f0d241965f9a2e62c2b
Colin McKean <colinmckeandigital@gmail.com>
2017-04-25 22:37:39 +0100

    added some css

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -18,21 +18,48 @@ export default class UI{
 
   showImage(recipe) {
 
-    var rc = document.querySelector('#recipe-container')
+    var rc = document.querySelector('#image-container')
     rc.innerHTML = '';
     var img = document.createElement('img');
     img.src = recipe.recipe.imageURL
-    img.width = 500;
 
     rc.appendChild(img);
-    // console.log(recipe.recipe.imageURL)
   }
 
   showRecipe(recipe) {
     var rc = document.querySelector('#recipe-container')
     rc.innerHTML = '';
     var h2 = document.createElement('h2');
+    // var instructions = document.createElement('p');
+    var ingredients = document.createElement('ul');
+    recipe.recipe.ingredients.forEach((ingredient)=> {
+      var li = document.createElement('li')
+      li.innerText = ingredient
+      ingredients.appendChild(li);
+    });
+    var instructions = document.createElement('ol');
+    recipe.recipe.instructions.forEach((instrction)=> {
+      var li = document.createElement('li')
+      li.innerText = instrction
+      instructions.appendChild(li);
+    })
+    // instructions.innerText = recipe.recipe.instructions;
     h2.innerText = recipe.recipe.title;
+
+
+
+
+//     1800000
+// imageURL
+// :
+// "images/913bd8e3f499ad8e0c44cc3433475b0c1493127669331.jpeg"
+// ingredients
+// instructions
+// :
+//
+
     rc.appendChild(h2);
+    rc.appendChild(ingredients);
+    rc.appendChild(instructions);
   }
 };
--- server/models/recipe.js
+++ server/models/recipe.js
@@ -3,12 +3,12 @@ var mongoose = require('mongoose');
 var Recipe = mongoose.model('Recipe', {
   title: {
     type: String,
-    // required: true,
+    required: true,
     minlength: 1,
     trim: true
   },
   instructions: {
-    type: String,
+    type: Array,
     // required: true,
     trim: true
   },
